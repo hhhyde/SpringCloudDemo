6a01d7937b5bb32dff189862f938b1af9dbda752
hhhyde <kj8550@gmail.com>
2017-07-18 22:32:14 +0800

    添加 Rest API 框架 Swagger,查看路径 http://localhost:8081/swagger-ui.html

--- eureka-client/src/main/java/cn/kejt/EurekaClientApplication.java
+++ eureka-client/src/main/java/cn/kejt/EurekaClientApplication.java
@@ -3,12 +3,16 @@ package cn.kejt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableDiscoveryClient
+
+/**
+ * 在 Application 启动类中支持 Swagger, 增加@EnableSwagger2注解, 让 SpringBoot 项目支持 Swagger
+ */
+@EnableSwagger2
 @SpringBootApplication
 public class EurekaClientApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
 }
--- eureka-client/src/main/java/cn/kejt/HelloController.java
+++ eureka-client/src/main/java/cn/kejt/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,7 +16,7 @@ public class HelloController {
     @Autowired
     private DiscoveryClient client;
 
-    @RequestMapping("hello")
+    @RequestMapping(value = "hello",method = RequestMethod.GET)
     public String index() {
         ServiceInstance instance = client.getLocalServiceInstance();
         logger.info("/hello,host:" + instance.getHost() + ",service_id:" + instance.getServiceId());
