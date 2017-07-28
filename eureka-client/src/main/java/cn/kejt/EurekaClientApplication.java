6cd2e060a4d294fa9a3415021f6771c583de8805
hhhyde <333333>
2017-07-28 11:40:43 +0800

    去掉 Swagger 支持,有bug

--- eureka-client/src/main/java/cn/kejt/EurekaClientApplication.java
+++ eureka-client/src/main/java/cn/kejt/EurekaClientApplication.java
@@ -3,13 +3,9 @@ package cn.kejt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
-/**
- * 在 Application 启动类中支持 Swagger, 增加@EnableSwagger2注解, 让 SpringBoot 项目支持 Swagger
- */
-@EnableSwagger2
+@EnableDiscoveryClient
 @SpringBootApplication
 public class EurekaClientApplication {
     public static void main(String[] args) {
--- eureka-client/src/main/java/cn/kejt/HelloController.java
+++ eureka-client/src/main/java/cn/kejt/HelloController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,7 +15,7 @@ public class HelloController {
     @Autowired
     private DiscoveryClient client;
 
-    @RequestMapping(value = "hello",method = RequestMethod.GET)
+    @RequestMapping("hello")
     public String index() {
         ServiceInstance instance = client.getLocalServiceInstance();
         logger.info("/hello,host:" + instance.getHost() + ",service_id:" + instance.getServiceId());
