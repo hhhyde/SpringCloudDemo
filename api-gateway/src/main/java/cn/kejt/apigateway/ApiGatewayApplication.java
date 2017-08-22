b037d9f2e659b7914c4870be698056d3bb3013cd
hhhyde <AziNe1lYQd9r>
2017-08-22 18:06:53 +0800

    Zuul 网管服务添加过滤器

--- api-gateway/src/main/java/cn/kejt/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/cn/kejt/apigateway/ApiGatewayApplication.java
@@ -1,13 +1,31 @@
 package cn.kejt.apigateway;
 
+import cn.kejt.apigateway.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
+/**
+ * 路由网关优点
+ * 1. 统一入口<br/>
+ * 2. 与服务治理框架结合,实现自动化的服务维护和负载均衡的路由转发<br/>
+ * 3. 可以将 接口权限验证 与 微服务业务部分 解耦<br/>
+ * 4. 通过过滤器,在各生命周期中校验请求,将原本在对外服务层做的校验前移,保证了为服务的无状态性,同时降低了微服务的测试难度,让服务本身更集中关注业务逻辑的处理<br/>
+ */
 @EnableZuulProxy
 @SpringCloudApplication
 public class ApiGatewayApplication {
+
+    /**
+     * 启动过滤器
+     * @return
+     */
+    @Bean
+    public AccessFilter accessFilter() {
+        return new AccessFilter();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
---
+++ api-gateway/src/main/java/cn/kejt/apigateway/filter/AccessFilter.java
@@ -0,0 +1,69 @@
+package cn.kejt.apigateway.filter;
+
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 过滤器,
+ * @implNote 在主类启动之后,才生效
+ */
+public class AccessFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(AccessFilter.class);
+
+    /**
+     * 过滤器的类型,决定了在请求的哪个生命周期进行过滤
+     * @return
+     */
+    @Override
+    public String filterType() {
+        // pre 表示 请求被路由之前执行
+        return "pre";
+    }
+
+    /**
+     * 过滤器的执行顺序,当请求在一个阶段中存在多个过滤器时,定义执行顺序
+     * @return
+     */
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    /**
+     * 判断该过滤器<code>是否需要</code>被执行.实际中可以利用该方法来指定过滤器的有效范围
+     * @return
+     */
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+
+        log.info("send {} request to {}", request.getMethod(), request.getRequestURL().toString());
+
+        Object accessToken = request.getParameter("accessToken");
+        if (accessToken == null) {
+            log.warn("access token is empty");
+            // 不进入路由
+            ctx.setSendZuulResponse(false);
+            // 返回 401 错误
+            ctx.setResponseStatusCode(401);
+            // optional
+            ctx.setResponseBody("可添加错误描述");
+            return null;
+        }
+        log.info("access token ok");
+        return null;
+    }
+
+}
