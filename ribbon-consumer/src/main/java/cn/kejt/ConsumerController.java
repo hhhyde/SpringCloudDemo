2c50c11354c964a7d68e2ab9b338edcc4512896f
hhhyde <AziNe1lYQd9r>
2017-09-16 18:07:36 +0800

    重写

--- ribbon-consumer/src/main/java/cn/kejt/ConsumerController.java
+++ ribbon-consumer/src/main/java/cn/kejt/ConsumerController.java
@@ -1,9 +1,11 @@
 package cn.kejt;
 
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,9 +14,17 @@ public class ConsumerController {
 
     @Autowired
     HelloService helloService;
+    @Autowired
+    RestTemplate restTemplate;
 
     @RequestMapping(value = "/ribbon-consumer", method = RequestMethod.GET)
     public String helloConsumer() {
-        return helloService.hello();
+        return helloService.helloService();
+    }
+
+    @RequestMapping(value = "/obd", method = RequestMethod.GET)
+    public String obd(@RequestParam(value = "key") String key) {
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
+        return restTemplate.getForEntity("http://OBDSERVICE/obd?key="+key, String.class).getBody();
     }
 }
