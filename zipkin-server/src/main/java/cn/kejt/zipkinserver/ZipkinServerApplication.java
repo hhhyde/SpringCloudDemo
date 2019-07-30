423a0759dc693ee20e737bbfbea60c576b4589f7
hhhyde <333333>
2019-07-30 16:58:14 +0800

    zipkin完成

--- zipkin-server/src/main/java/cn/kejt/zipkinserver/ZipkinServerApplication.java
+++ zipkin-server/src/main/java/cn/kejt/zipkinserver/ZipkinServerApplication.java
@@ -2,9 +2,11 @@ package cn.kejt.zipkinserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import zipkin.server.EnableZipkinServer;
 
 @EnableZipkinServer
+@EnableEurekaClient
 @SpringBootApplication
 public class ZipkinServerApplication {
 
